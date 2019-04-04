06d9b7265235aab937454074a27295778de4002e
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2019-04-04 22:57:24 +0300

    WIP

--- examples/canny-edge-detector-in-cnn/example_canny_cnn.py
+++ examples/canny-edge-detector-in-cnn/example_canny_cnn.py
@@ -8,6 +8,7 @@ from catalyst.contrib.criterion import FocalLoss
 from catalyst.dl.callbacks import EarlyStoppingCallback, JaccardCallback, Callback, \
     RunnerState, TensorboardLogger
 from catalyst.dl.experiments import SupervisedRunner
+from catalyst.dl.utils import UtilsFactory
 from sklearn.model_selection import train_test_split
 from torch import nn
 from torch.backends import cudnn
@@ -42,14 +43,52 @@ class PoolUnpool(nn.Module):
         return unpuul
 
 
+class PolarGradients(nn.Module):
+    def __init__(self, input_channels):
+        super().__init__()
+        self.gray = nn.Conv2d(input_channels, 1, kernel_size=1)
+        self.smooth = nn.Conv2d(1, 1, kernel_size=5, padding=2)
+        self.dx = nn.Conv2d(1, 1, kernel_size=3, padding=1, bias=False)
+        self.dy = nn.Conv2d(1, 1, kernel_size=3, padding=1, bias=False)
+
+        gaussian_kernel = np.array([[1, 4, 7, 4, 1],
+                                    [4, 16, 26, 16, 4],
+                                    [7, 26, 41, 26, 7],
+                                    [4, 16, 26, 16, 4],
+                                    [1, 4, 7, 4, 1]], dtype=np.float32) / 273.
+
+        dx_kernel = np.array([[1, 0, -1],
+                              [2, 0, -2],
+                              [1, 0, -1]], dtype=np.float32)
+
+        dy_kernel = np.array([[-1, -2, -1],
+                              [0, 0, 0],
+                              [1, 2, 1]], dtype=np.float32)
+
+        with torch.no_grad():
+            self.smooth.weight.set_(torch.from_numpy(gaussian_kernel).unsqueeze(0).unsqueeze(0))
+            self.dx.weight.set_(torch.from_numpy(dx_kernel).unsqueeze(0).unsqueeze(0))
+            self.dy.weight.set_(torch.from_numpy(dy_kernel).unsqueeze(0).unsqueeze(0))
+
+    def forward(self, x):
+        x = self.gray(x)
+        x = self.smooth(x)
+        dx = self.dx(x)
+        dy = self.dy(x)
+        mag = torch.sqrt(dx * dx + dy * dy + 1e-4)
+        ori = torch.atan2(dy, dx)
+        return torch.cat([mag, ori], dim=1)
+
+
 class CannyModel(nn.Module):
     def __init__(self, input_channels=3, features=16):
         super().__init__()
+        self.polar_grad = PolarGradients(input_channels)
         self.conv1 = conv_bn_relu(input_channels, features)
         self.conv2 = conv_bn_relu(features, features)
 
-        self.conv3 = conv_bn_relu(features * 2, features)
-        self.conv4 = conv_bn_relu(features * 2, features)
+        self.conv3 = conv_bn_relu(features * 2 + 2, features)
+        self.conv4 = conv_bn_relu(features * 2 + 2, features)
 
         self.pool1 = PoolUnpool(features)
         self.pool2 = PoolUnpool(features)
@@ -57,15 +96,16 @@ class CannyModel(nn.Module):
         self.final = nn.Conv2d(features, 1, kernel_size=1)
 
     def forward(self, x):
+        pg = self.polar_grad(x)
         x = self.conv1(x)
         x = self.conv2(x)
 
         p1 = self.pool1(x)
-        x = torch.cat([x, p1], dim=1)
+        x = torch.cat([x, p1, pg], dim=1)
         x = self.conv3(x)
 
         p2 = self.pool2(x)
-        x = torch.cat([x, p2], dim=1)
+        x = torch.cat([x, p2, pg], dim=1)
         x = self.conv4(x)
 
         x = self.final(x)
@@ -102,6 +142,7 @@ class EdgesDataset(Dataset):
         data = self.normalize(**data)
         data['image'] = tensor_from_rgb_image(data['image'])
         data['mask'] = torch.from_numpy(data['mask']).float().unsqueeze(0)
+        
         return {'features': data['image'], 'targets': data['mask']}
 
 
@@ -148,7 +189,7 @@ class ShowPolarBatchesCallback(Callback):
             if isinstance(logger, TensorboardLogger):
                 logger.loggers[mode].add_image(f"{name}{suffix}", tensor_from_rgb_image(image), step)
 
-    def on_epoch_start(self, state):
+    def on_loader_start(self, state):
         self.best_score = None
         self.best_input = None
         self.best_output = None
@@ -170,22 +211,20 @@ class ShowPolarBatchesCallback(Callback):
             self.worst_input = self.to_cpu(state.input)
             self.worst_output = self.to_cpu(state.output)
 
-    def on_epoch_end(self, state: RunnerState) -> None:
+    def on_loader_end(self, state: RunnerState) -> None:
         mode = state.loader_name
 
         if self.best_score is not None:
             best_samples = self.visualize_batch(self.best_input, self.best_output)
             for i, image in enumerate(best_samples):
                 self._log_image(state.loggers, mode, image, f"Best Batch/{i}", state.step, suffix="/epoch")
-                cv2.imshow("Best sample " + str(i), cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
+                # cv2.imshow("Best sample " + str(i), cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
 
         if self.worst_score is not None:
             worst_samples = self.visualize_batch(self.worst_input, self.worst_output)
             for i, image in enumerate(worst_samples):
                 self._log_image(state.loggers, mode, image, f"Worst Batch/{i}", state.step, suffix="/epoch")
-                cv2.imshow("Worst sample " + str(i), cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
-
-        cv2.waitKey(1000)
+                # cv2.imshow("Worst sample " + str(i), cv2.cvtColor(image, cv2.COLOR_RGB2BGR))
 
 
 def visualize_canny_predictions(input, output, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
@@ -195,9 +234,9 @@ def visualize_canny_predictions(input, output, mean=(0.485, 0.456, 0.406), std=(
         target = to_numpy(target).squeeze(0)
         logits = to_numpy(logits.sigmoid()).squeeze(0)
 
-        overlay = np.ones_like(image) * (255, 0, 0)  # Full red image
-        overlay = overlay * np.expand_dims(logits, -1)
-        overlay[target > 0] = (0, 255, 0)
+        overlay = np.zeros_like(image)
+        overlay[logits > 0.5] += np.array([255, 0, 0], dtype=overlay.dtype)
+        overlay[target > 0] += np.array([0, 255, 0], dtype=overlay.dtype)
 
         overlay = cv2.addWeighted(image, 0.5, overlay, 0.5, 0, dtype=cv2.CV_8U)
         images.append(overlay)
@@ -208,16 +247,17 @@ def main():
     images_dir = 'd:\datasets\mirflickr'
 
     canny_cnn = maybe_cuda(CannyModel())
-    optimizer = Adam(canny_cnn.parameters(), lr=1e-3)
+    optimizer = Adam(canny_cnn.parameters(), lr=1e-5)
 
     images = find_images_in_dir(images_dir)
     train_images, valid_images = train_test_split(images, test_size=0.1, random_state=1234)
 
-    num_workers = 0
+    num_workers = 8
     num_epochs = 100
     batch_size = 128
 
-    if True:
+    if False:
+        num_workers = 0
         batch_size = 32
         train_images = train_images[:batch_size * 4]
         valid_images = valid_images[:batch_size * 4]
@@ -235,7 +275,6 @@ def main():
 
     # model runner
     runner = SupervisedRunner()
-
     # checkpoint = UtilsFactory.load_checkpoint("logs/checkpoints//best.pth")
     # UtilsFactory.unpack_checkpoint(checkpoint, model=canny_cnn)
 
