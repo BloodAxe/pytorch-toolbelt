c5eb86601b48fb687eac52a0a1388b8c87f28386
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2019-04-05 23:03:11 +0300

    Update build scripts

--- setup.py
+++ setup.py
@@ -17,12 +17,17 @@ import sys
 
 from setuptools import find_packages, setup
 
-DEPENDENCIES = ["torch>=0.4.1", "torchvision>0.2"]
-EXCLUDE_FROM_PACKAGES = ["contrib", "docs", "tests*"]
-CURDIR = os.path.abspath(os.path.dirname(__file__))
+# Package meta-data.
+NAME = "pytorch_toolbelt"
+DESCRIPTION = "PyTorch extensions for fast R&D prototyping and Kaggle farming"
+URL = "https://github.com/BloodAxe/pytorch-toolbelt"
+EMAIL = "ekhvedchenya@gmail.com"
+AUTHOR = "Eugene Khvedchenya"
+REQUIRES_PYTHON = ">=3.6.0"
 
-with io.open(os.path.join(CURDIR, "README.md"), "r", encoding="utf-8") as f:
-    README = f.read()
+DEPENDENCIES = ["torch>=0.4.1", "torchvision>0.2"]
+EXCLUDE_FROM_PACKAGES = ["contrib", "docs", "tests", "examples"]
+CURDIR = os.path.abspath(os.path.dirname(__file__))
 
 
 def get_version():
@@ -34,6 +39,12 @@ def get_version():
     return str(ast.literal_eval(version))
 
 
+def load_readme():
+    readme_path = os.path.join(CURDIR, "README.md")
+    with io.open(readme_path, encoding="utf-8") as f:
+        return "\n" + f.read()
+
+
 def get_test_requirements():
     requirements = ['pytest']
     if sys.version_info < (3, 3):
@@ -42,24 +53,21 @@ def get_test_requirements():
 
 
 setup(
-    name="pytorch_toolbelt",
+    name=NAME,
     version=get_version(),
-    author="Eugene Khvedchenya",
-    author_email="ekhvedchenya@gmail.com",
-    description="PyTorch extensions for fast R&D prototyping and Kaggle farming",
-    long_description=README,
+    author=AUTHOR,
+    author_email=EMAIL,
+    description=DESCRIPTION,
+    long_description=load_readme(),
     long_description_content_type="text/markdown",
-    url="https://github.com/BloodAxe/pytorch-toolbelt",
+    url=URL,
     packages=find_packages(exclude=EXCLUDE_FROM_PACKAGES),
+    install_requires=DEPENDENCIES,
+    python_requires=REQUIRES_PYTHON,
     extras_require={'tests': get_test_requirements()},
     include_package_data=True,
     keywords=["PyTorch", "Kaggle", "Deep Learning", "Machine Learning", "ResNet", "VGG", "ResNext", "Unet", "Focal"],
     scripts=[],
-    zip_safe=False,
-    install_requires=DEPENDENCIES,
-    python_requires=">=3.6",
-    # license and classifier list:
-    # https://pypi.org/pypi?%3Aaction=list_classifiers
     license="License :: OSI Approved :: MIT License",
     classifiers=[
         'Development Status :: 4 - Beta',
@@ -67,9 +75,12 @@ setup(
         'Intended Audience :: Science/Research',
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
         "Topic :: Scientific/Engineering :: Mathematics",
-        "Topic :: Scientific/Engineering :: Image Recognition"
+        "Topic :: Scientific/Engineering :: Image Recognition",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         "Topic :: Software Development :: Libraries :: Application Frameworks"
