1e048a860328204046310fb0c4ad8d6334849e36
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2019-04-06 00:14:12 +0300

    Add inits

--- pytorch_toolbelt/__init__.py
+++ pytorch_toolbelt/__init__.py
@@ -1,9 +1,3 @@
 from __future__ import absolute_import
 
 __version__ = '0.0.2'
-
-from .inference import *
-from .losses import *
-from .modules import *
-from .optimization import *
-from .utils import *
