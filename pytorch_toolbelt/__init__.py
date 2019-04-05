9f997730dedb6866393fde714be9d05cc84cce0f
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2019-04-05 23:59:48 +0300

    Add confusion matrix visualization

--- pytorch_toolbelt/__init__.py
+++ pytorch_toolbelt/__init__.py
@@ -1,3 +1,9 @@
 from __future__ import absolute_import
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
+
+from .inference import *
+from .losses import *
+from .modules import *
+from .optimization import *
+from .utils import *
