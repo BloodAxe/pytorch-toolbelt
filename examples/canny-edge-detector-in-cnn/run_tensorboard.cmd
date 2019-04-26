d8f2d452fb6bfe4ea6587eac04b221d06ab8d249
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2019-04-26 11:43:57 +0300

    Remove examples (move to separate repo)

--- examples/canny-edge-detector-in-cnn/run_tensorboard.cmd
+++
@@ -1,3 +0,0 @@
-@call c:\Anaconda3\Scripts\activate.bat tb
-set CUDA_VISIBLE_DEVICES=
-tensorboard --logdir logs --host 0.0.0.0
--- examples/segmentation-inria/run_tensorboard.cmd
+++
@@ -1,3 +0,0 @@
-@call c:\Anaconda3\Scripts\activate.bat tb
-set CUDA_VISIBLE_DEVICES=
-tensorboard --logdir runs --host 0.0.0.0 --port 5555
