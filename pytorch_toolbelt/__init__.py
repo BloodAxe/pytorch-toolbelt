a3f52fdbf0b797ccf53dbab9376a3768712a8aa9
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2019-04-26 18:27:03 +0300

    Added 10-Crop TTA

--- pytorch_toolbelt/__init__.py
+++ pytorch_toolbelt/__init__.py
@@ -1,3 +1,3 @@
 from __future__ import absolute_import
 
-__version__ = '0.0.4'
+__version__ = '0.0.5'
---
+++ tests/test_tta.py
@@ -0,0 +1,89 @@
+import torch
+import numpy as np
+from pytorch_toolbelt.inference import tta
+from pytorch_toolbelt.utils.torch_utils import to_numpy
+from torch import nn
+
+
+class NoOp(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return input
+
+
+class SumAll(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, input):
+        return input.sum(dim=[1, 2, 3])
+
+
+def test_d4_image2mask():
+    input = torch.rand((4, 3, 224, 224))
+    model = NoOp()
+
+    output = tta.d4_image2mask(model, input)
+    np.testing.assert_allclose(to_numpy(output), to_numpy(input), atol=1e-6, rtol=1e-6)
+
+
+def test_fliplr_image2mask():
+    input = torch.rand((4, 3, 224, 224))
+    model = NoOp()
+
+    output = tta.fliplr_image2mask(model, input)
+    np.testing.assert_allclose(to_numpy(output), to_numpy(input), atol=1e-6, rtol=1e-6)
+
+
+def test_d4_image2label():
+    input = torch.tensor([[1, 2, 3, 4],
+                          [5, 6, 7, 8],
+                          [9, 0, 1, 2],
+                          [3, 4, 5, 6]]).unsqueeze(0).unsqueeze(0).float()
+    model = SumAll()
+
+    output = tta.d4_image2label(model, input)
+    expected = int(input.sum())
+
+    assert int(output) == expected
+
+
+def test_fliplr_image2label():
+    input = torch.tensor([[1, 2, 3, 4],
+                          [5, 6, 7, 8],
+                          [9, 0, 1, 2],
+                          [3, 4, 5, 6]]).unsqueeze(0).unsqueeze(0).float()
+    model = SumAll()
+
+    output = tta.fliplr_image2label(model, input)
+    expected = int(input.sum())
+
+    assert int(output) == expected
+
+
+def test_fivecrop_image2label():
+    input = torch.tensor([[1, 2, 3, 4],
+                          [5, 6, 7, 8],
+                          [9, 0, 1, 2],
+                          [3, 4, 5, 6]]).unsqueeze(0).unsqueeze(0).float()
+    model = SumAll()
+
+    output = tta.fivecrop_image2label(model, input, (2, 2))
+    expected = ((1 + 2 + 5 + 6) + (3 + 4 + 7 + 8) + (9 + 0 + 3 + 4) + (1 + 2 + 5 + 6) + (6 + 7 + 0 + 1)) / 5
+
+    assert int(output) == expected
+
+
+def test_tencrop_image2label():
+    input = torch.tensor([[1, 2, 3, 4],
+                          [5, 6, 7, 8],
+                          [9, 0, 1, 2],
+                          [3, 4, 5, 6]]).unsqueeze(0).unsqueeze(0).float()
+    model = SumAll()
+
+    output = tta.tencrop_image2label(model, input, (2, 2))
+    expected = (2 * ((1 + 2 + 5 + 6) + (3 + 4 + 7 + 8) + (9 + 0 + 3 + 4) + (1 + 2 + 5 + 6) + (6 + 7 + 0 + 1))) / 10
+
+    assert int(output) == expected
