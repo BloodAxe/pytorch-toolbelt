4b8e23ecd523f0a0c6834934ea3ac8484dc02a9b
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2019-09-27 23:02:01 +0300

    Improve tests

--- tests/test_tiles.py
+++ tests/test_tiles.py
@@ -1,42 +1,50 @@
 import numpy as np
 import torch
 from pytorch_toolbelt.inference.tiles import ImageSlicer, CudaTileMerger
-from pytorch_toolbelt.utils.torch_utils import tensor_from_rgb_image, \
-    rgb_image_from_tensor, to_numpy
+from pytorch_toolbelt.utils.torch_utils import (
+    tensor_from_rgb_image,
+    rgb_image_from_tensor,
+    to_numpy,
+)
 from torch import nn
 from torch.utils.data import DataLoader
 
 
 def test_tiles_split_merge():
-    image = np.random.random((5000, 5000, 3)).astype(np.uint8)
-    tiler = ImageSlicer(image.shape, tile_size=512, tile_step=256,
-                        weight='mean')
+    image = np.random.random((500, 500, 3)).astype(np.uint8)
+    tiler = ImageSlicer(image.shape, tile_size=51, tile_step=26, weight="mean")
     tiles = tiler.split(image)
     merged = tiler.merge(tiles, dtype=np.uint8)
     np.testing.assert_equal(merged, image)
 
 
 def test_tiles_split_merge_non_dividable():
-    image = np.random.random((5632, 5120, 3)).astype(np.uint8)
-    tiler = ImageSlicer(image.shape, tile_size=(1280, 1280),
-                        tile_step=(1280, 1280), weight='mean')
+    image = np.random.random((563, 512, 3)).astype(np.uint8)
+    tiler = ImageSlicer(
+        image.shape, tile_size=(128, 128), tile_step=(128, 128), weight="mean"
+    )
     tiles = tiler.split(image)
     merged = tiler.merge(tiles, dtype=np.uint8)
     np.testing.assert_equal(merged, image)
 
 
 def test_tiles_split_merge_non_dividable_cuda():
+    if not torch.cuda.is_available():
+        return
     image = np.random.random((5632, 5120, 3)).astype(np.uint8)
-    tiler = ImageSlicer(image.shape, tile_size=(1280, 1280),
-                        tile_step=(1280, 1280), weight='mean')
+    tiler = ImageSlicer(
+        image.shape, tile_size=(1280, 1280), tile_step=(1280, 1280), weight="mean"
+    )
     tiles = tiler.split(image)
 
-    merger = CudaTileMerger(tiler.target_shape, channels=image.shape[2],
-                            weight=tiler.weight)
+    merger = CudaTileMerger(
+        tiler.target_shape, channels=image.shape[2], weight=tiler.weight
+    )
     for tile, coordinates in zip(tiles, tiler.crops):
         # Integrate as batch of size 1
         merger.integrate_batch(
-            tensor_from_rgb_image(tile).unsqueeze(0).float().cuda(), [coordinates])
+            tensor_from_rgb_image(tile).unsqueeze(0).float().cuda(), [coordinates]
+        )
 
     merged = merger.merge()
     merged = rgb_image_from_tensor(merged, mean=0, std=1, max_pixel_value=1)
@@ -47,8 +55,9 @@ def test_tiles_split_merge_non_dividable_cuda():
 
 def test_tiles_split_merge_2():
     image = np.random.random((5000, 5000, 3)).astype(np.uint8)
-    tiler = ImageSlicer(image.shape, tile_size=(512, 512),
-                        tile_step=(256, 256), weight='pyramid')
+    tiler = ImageSlicer(
+        image.shape, tile_size=(512, 512), tile_step=(256, 256), weight="pyramid"
+    )
 
     np.testing.assert_allclose(tiler.weight, tiler.weight.T)
 
@@ -71,15 +80,17 @@ def test_tiles_split_merge_cuda():
             return max_channel
 
     image = np.random.random((5000, 5000, 3)).astype(np.uint8)
-    tiler = ImageSlicer(image.shape, tile_size=(512, 512),
-                        tile_step=(256, 256), weight='pyramid')
+    tiler = ImageSlicer(
+        image.shape, tile_size=(512, 512), tile_step=(256, 256), weight="pyramid"
+    )
     tiles = [tensor_from_rgb_image(tile) for tile in tiler.split(image)]
 
     model = MaxChannelIntensity().eval().cuda()
 
     merger = CudaTileMerger(tiler.target_shape, 1, tiler.weight)
-    for tiles_batch, coords_batch in DataLoader(list(zip(tiles, tiler.crops)),
-                                                batch_size=8, pin_memory=True):
+    for tiles_batch, coords_batch in DataLoader(
+        list(zip(tiles, tiler.crops)), batch_size=8, pin_memory=True
+    ):
         tiles_batch = tiles_batch.float().cuda()
         pred_batch = model(tiles_batch)
 
