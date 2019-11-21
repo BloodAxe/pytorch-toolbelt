b94fad7cf6ae68bf2a5a831460c193f0ac269212
Eugene Khvedchenya <ekhvedchenya@gmail.com>
2019-11-21 13:48:21 +0200

    Fix SoftBCELoss

--- pytorch_toolbelt/losses/soft_bce.py
+++ pytorch_toolbelt/losses/soft_bce.py
@@ -6,20 +6,23 @@ __all__ = ["SoftBCELoss"]
 
 
 class SoftBCELoss(nn.Module):
-    def __init__(self, smooth_factor=1e-4, ignore_index=None, reduction="mean"):
+    def __init__(self, smooth_factor=None, ignore_index=None, reduction="mean"):
         super().__init__()
         self.smooth_factor = smooth_factor
         self.ignore_index = ignore_index
         self.reduction = reduction
 
     def forward(self, label_input, label_target):
-        not_ignored_mask = label_target != self.ignore_index
+        if self.ignore_index is not None:
+            not_ignored_mask = (label_target != self.ignore_index).float()
 
-        label_target = (1 - label_target) * self.smooth_factor + label_target * (1 - self.smooth_factor)
+        if self.smooth_factor is not None:
+            label_target = (1 - label_target) * self.smooth_factor + label_target * (1 - self.smooth_factor)
 
         loss = F.binary_cross_entropy_with_logits(label_input, label_target, reduction="none")
 
-        loss = loss * not_ignored_mask.float()
+        if self.ignore_index is not None:
+            loss = loss * not_ignored_mask.float()
 
         if self.reduction == "mean":
             loss = loss.mean()
