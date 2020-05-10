741d150684ce63507674f78289874af74bed68a1
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2020-05-10 12:06:40 +0300

    Add temperature scaling for softmax & sigmoid activations

--- pytorch_toolbelt/inference/ensembling.py
+++ pytorch_toolbelt/inference/ensembling.py
@@ -5,39 +5,54 @@ __all__ = ["ApplySoftmaxTo", "ApplySigmoidTo", "Ensembler", "PickModelOutput"]
 
 
 class ApplySoftmaxTo(nn.Module):
-    def __init__(self, model, output_key: Union[str, List[str]] = "logits", dim=1):
+    def __init__(self, model: nn.Module, output_key: Union[str, List[str]] = "logits", dim=1, temperature=1):
+        """
+        Apply softmax activation on given output(s) of the model
+        :param model: Model to wrap
+        :param output_key: string or list of strings, indicating to what outputs softmax activation should be applied.
+        :param dim: Tensor dimension for softmax activation
+        :param temperature: Temperature scaling coefficient. Values > 1 will make logits sharper.
+        """
         super().__init__()
         output_key = output_key if isinstance(output_key, (list, tuple)) else [output_key]
         # By converting to set, we prevent double-activation by passing output_key=["logits", "logits"]
         self.output_keys = set(output_key)
         self.model = model
         self.dim = dim
+        self.temperature = temperature
 
     def forward(self, *input, **kwargs):
         output = self.model(*input, **kwargs)
         for key in self.output_keys:
-            output[key] = output[key].softmax(dim=1)
+            output[key] = output[key].mul(self.temperature).softmax(dim=1)
         return output
 
 
 class ApplySigmoidTo(nn.Module):
-    def __init__(self, model, output_key: Union[str, List[str]] = "logits"):
+    def __init__(self, model: nn.Module, output_key: Union[str, List[str]] = "logits", temperature=1):
+        """
+        Apply sigmoid activation on given output(s) of the model
+        :param model: Model to wrap
+        :param output_key: string or list of strings, indicating to what outputs sigmoid activation should be applied.
+        :param temperature: Temperature scaling coefficient. Values > 1 will make logits sharper.
+        """
         super().__init__()
         output_key = output_key if isinstance(output_key, (list, tuple)) else [output_key]
         # By converting to set, we prevent double-activation by passing output_key=["logits", "logits"]
         self.output_keys = set(output_key)
         self.model = model
+        self.temperature = temperature
 
     def forward(self, *input, **kwargs):  # skipcq: PYL-W0221
         output = self.model(*input, **kwargs)
         for key in self.output_keys:
-            output[key] = output[key].sigmoid()
+            output[key] = output[key].mul(self.temperature).sigmoid()
         return output
 
 
 class Ensembler(nn.Module):
     """
-    Computes sum of outputs for several models with arithmetic averaging (optional).
+    Compute sum (or average) of outputs of several models.
     """
 
     def __init__(self, models: List[nn.Module], average=True, outputs=None):
