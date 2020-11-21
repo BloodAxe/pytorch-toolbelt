1c87f230c5633c880c117f8e11eb9e82efa5d73e
BloodAxe <ekhvedchenya@gmail.com>
2020-11-21 22:22:14 +0200

    Register modules & losses in Catalyst registry

--- pytorch_toolbelt/utils/catalyst/__init__.py
+++ pytorch_toolbelt/utils/catalyst/__init__.py
@@ -7,3 +7,21 @@ from .opl import *
 from .visualization import *
 from .utils import *
 from .loss_adapter import *
+
+from catalyst.registry import MODULE, Registry, CRITERION
+
+
+def _register_modules(r: Registry):
+    from pytorch_toolbelt.modules import encoders as e
+
+    r.add_from_module(e, prefix="tbt")
+
+
+def _register_criterions(r: Registry):
+    from pytorch_toolbelt import losses as l
+
+    r.add_from_module(e, prefix="tbt")
+
+
+MODULE.late_add(_register_modules)
+CRITERION.late_add(_register_criterions)
