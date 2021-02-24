036d144fa6ed6d9e4d9f78f5dc940ded5952e91b
BloodAxe <ekhvedchenya@gmail.com>
2021-02-24 23:05:56 +0200

    RocAucMetricCallback

--- pytorch_toolbelt/utils/catalyst/callbacks/roc_auc.py
+++ pytorch_toolbelt/utils/catalyst/callbacks/roc_auc.py
@@ -1,12 +1,20 @@
+from typing import Callable, Optional
+
 import numpy as np
+import torch
+from catalyst.core import Callback, CallbackOrder
 from sklearn.metrics import roc_auc_score
+from torch import Tensor
 
 __all__ = ["RocAucMetricCallback"]
+
+from pytorch_toolbelt.utils import to_numpy
+from pytorch_toolbelt.utils.distributed import all_gather
 
 
 class RocAucMetricCallback(Callback):
     """
-    Accuracy score metric for multi-label case (aka Exact Match Ratio, Subset accuracy).
+    Roc Auc score metric
     """
 
     def __init__(
@@ -25,8 +33,6 @@ class RocAucMetricCallback(Callback):
             output_key: output key to use for accuracy calculation;
                 specifies our `y_pred`
             prefix: key for the metric's name
-            num_classes: number of classes to calculate ``topk_args``
-                if ``accuracy_args`` is None
         """
 
         super().__init__(CallbackOrder.Metric)
@@ -37,20 +43,21 @@ class RocAucMetricCallback(Callback):
         self.outputs_to_probas = outputs_to_probas
         self.y_trues = []
         self.y_preds = []
+        self.average = average
 
     def on_loader_start(self, state):
         self.y_trues = []
         self.y_preds = []
 
     @torch.no_grad()
-    def on_batch_end(self, runner: IRunner):
+    def on_batch_end(self, runner):
         pred_probas = self.outputs_to_probas(runner.output[self.output_key])
-        true_labels = runner.input[self.input_key].type_as(pred_labels)
+        true_labels = runner.input[self.input_key]
 
         self.y_trues.extend(to_numpy(true_labels))
         self.y_preds.extend(to_numpy(pred_probas))
 
-    def on_loader_end(self, runner: IRunner):
+    def on_loader_end(self, runner):
         y_trues = np.concatenate(all_gather(self.y_trues))
         y_preds = np.concatenate(all_gather(self.y_preds))
         score = roc_auc_score(y_true=y_trues, y_score=y_preds, average=self.average)
