90571f749ce6d76ec542f9a2397ad1991d32f831
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2021-06-01 16:26:39 +0300

    Refactor DeeplabV3 decoders

--- pytorch_toolbelt/modules/decoders/deeplab.py
+++ pytorch_toolbelt/modules/decoders/deeplab.py
@@ -1,121 +1,233 @@
-from typing import List
+from collections import OrderedDict
+from typing import List, Tuple
 
 import torch
-from torch import nn, Tensor
 import torch.nn.functional as F
+from torch import nn, Tensor
 
 from .common import DecoderModule
-from ..activations import ABN
+from ..dsconv import DepthwiseSeparableConv2d
+from ..activations import instantiate_activation_block, ACT_RELU
 
-__all__ = ["DeeplabV3Decoder"]
+__all__ = ["ASPPModule", "SeparableASPPModule", "DeeplabV3Decoder", "DeeplabV3PlusDecoder"]
 
 
 class ASPPModule(nn.Module):
-    def __init__(self, inplanes, planes, kernel_size, padding, dilation, abn_block=ABN):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        padding: int,
+        dilation: int,
+        norm_layer=nn.BatchNorm2d,
+        activation=ACT_RELU,
+    ):
         super(ASPPModule, self).__init__()
-        self.atrous_conv = nn.Conv2d(
-            inplanes, planes, kernel_size=kernel_size, stride=1, padding=padding, dilation=dilation, bias=False
+        self.conv = nn.Conv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=1,
+            padding=padding,
+            dilation=dilation,
+            bias=False,
         )
-        self.abn = abn_block(planes)
+        self.abn = nn.Sequential(
+            OrderedDict(
+                [("norm", norm_layer(out_channels)), ("act", instantiate_activation_block(activation, inplace=True))]
+            )
+        )
 
     def forward(self, x):  # skipcq: PYL-W0221
-        x = self.atrous_conv(x)
+        x = self.conv(x)
         x = self.abn(x)
         return x
 
 
-class ASPP(nn.Module):
-    def __init__(self, inplanes: int, output_stride: int, output_features: int, dropout=0.5, abn_block=ABN):
-        super(ASPP, self).__init__()
-
-        if output_stride == 32:
-            dilations = [1, 3, 6, 9]
-        elif output_stride == 16:
-            dilations = [1, 6, 12, 18]
-        elif output_stride == 8:
-            dilations = [1, 12, 24, 36]
-        else:
-            raise NotImplementedError
-
-        self.aspp1 = ASPPModule(inplanes, output_features, 1, padding=0, dilation=dilations[0])
-        self.aspp2 = ASPPModule(inplanes, output_features, 3, padding=dilations[1], dilation=dilations[1])
-        self.aspp3 = ASPPModule(inplanes, output_features, 3, padding=dilations[2], dilation=dilations[2])
-        self.aspp4 = ASPPModule(inplanes, output_features, 3, padding=dilations[3], dilation=dilations[3])
-
-        self.global_avg_pool = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Conv2d(inplanes, output_features, 1, stride=1, bias=False),
-            abn_block(output_features),
+class SeparableASPPModule(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        padding: int,
+        dilation: int,
+        norm_layer=nn.BatchNorm2d,
+        activation=ACT_RELU,
+    ):
+        super().__init__()
+        self.conv = DepthwiseSeparableConv2d(
+            in_channels,
+            out_channels,
+            kernel_size=kernel_size,
+            stride=1,
+            padding=padding,
+            dilation=dilation,
+            bias=False,
         )
-        self.conv1 = nn.Conv2d(output_features * 5, output_features, 1, bias=False)
-        self.abn1 = abn_block(output_features)
-        self.dropout = nn.Dropout(dropout)
+        self.abn = nn.Sequential(
+            OrderedDict(
+                [("norm", norm_layer(out_channels)), ("act", instantiate_activation_block(activation, inplace=True))]
+            )
+        )
 
     def forward(self, x):  # skipcq: PYL-W0221
-        x1 = self.aspp1(x)
-        x2 = self.aspp2(x)
-        x3 = self.aspp3(x)
-        x4 = self.aspp4(x)
-        x5 = self.global_avg_pool(x)
-        x5 = F.interpolate(x5, size=x4.size()[2:], mode="bilinear", align_corners=False)
-        x = torch.cat((x1, x2, x3, x4, x5), dim=1)
+        x = self.conv(x)
+        x = self.abn(x)
+        return x
 
-        x = self.conv1(x)
-        x = self.abn1(x)
 
-        return self.dropout(x)
+class ASPPPooling(nn.Sequential):
+    def __init__(self, in_channels, out_channels, norm_layer=nn.BatchNorm2d, activation=ACT_RELU):
+        super().__init__()
+
+        self.pooling = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv2d(in_channels, out_channels, 1, bias=False)
+        self.norm = nn.BatchNorm2d(out_channels)
+        self.act = instantiate_activation_block(activation, inplace=True)
+        self.abn = nn.Sequential(
+            OrderedDict(
+                [("norm", norm_layer(out_channels)), ("act", instantiate_activation_block(activation, inplace=True))]
+            )
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        size = x.shape[-2:]
+        x = self.pooling(x)
+        x = self.conv(x)
+        x = self.abn(x)
+        return F.interpolate(x, size=size, mode="bilinear", align_corners=False)
+
+
+class ASPP(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        atrous_rates=(12, 24, 36),
+        dropout: float = 0.5,
+        activation: str = ACT_RELU,
+        aspp_module=ASPPModule,
+    ):
+        super(ASPP, self).__init__()
+        aspp_modules = [
+            aspp_module(in_channels, out_channels, 3, padding=1, dilation=1, activation=activation),
+            ASPPPooling(in_channels, out_channels),
+        ] + [aspp_module(in_channels, out_channels, 3, padding=ar, dilation=ar) for ar in atrous_rates]
+
+        self.aspp = nn.ModuleList(aspp_modules)
+        self.project = nn.Sequential(
+            nn.Conv2d(len(self.aspp) * out_channels, out_channels, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            instantiate_activation_block(activation, inplace=False),
+            nn.Dropout(dropout, inplace=True),
+        )
+
+    def forward(self, x: Tensor) -> Tensor:  # skipcq: PYL-W0221
+        res = []
+        for aspp in self.aspp:
+            res.append(aspp(x))
+        res = torch.cat(res, dim=1)
+        return self.project(res)
 
 
 class DeeplabV3Decoder(DecoderModule):
+    """
+    Implements DeepLabV3 model from `"Rethinking Atrous Convolution for Semantic Image Segmentation"
+    <https://arxiv.org/abs/1706.05587>`_.
+
+    Partially copy-pasted from https://github.com/pytorch/vision/blob/master/torchvision/models/segmentation/deeplabv3.py
+    """
+
     def __init__(
         self,
         feature_maps: List[int],
-        num_classes: int,
-        output_stride=32,
-        high_level_bottleneck=256,
-        low_level_bottleneck=32,
-        dropout=0.5,
-        abn_block=ABN,
+        aspp_channels: int,
+        out_channels: int,
+        atrous_rates=(12, 24, 36),
+        dropout: float = 0.5,
+        activation=ACT_RELU,
     ):
-        super(DeeplabV3Decoder, self).__init__()
-
-        self.aspp = ASPP(feature_maps[-1], output_stride, high_level_bottleneck, dropout=dropout, abn_block=abn_block)
-
-        self.conv1 = nn.Conv2d(feature_maps[0], low_level_bottleneck, 1, bias=False)
-        self.abn1 = abn_block(low_level_bottleneck)
-
-        self.last_conv = nn.Sequential(
-            nn.Conv2d(
-                high_level_bottleneck + low_level_bottleneck,
-                high_level_bottleneck,
-                kernel_size=3,
-                padding=1,
-                bias=False,
-            ),
-            abn_block(high_level_bottleneck),
-            nn.Dropout(dropout),
-            nn.Conv2d(high_level_bottleneck, high_level_bottleneck, kernel_size=3, padding=1, bias=False),
-            abn_block(high_level_bottleneck),
-            nn.Dropout(dropout * 0.2),  # 5 times smaller dropout rate
-            nn.Conv2d(high_level_bottleneck, num_classes, kernel_size=1),
+        super().__init__()
+        self.aspp = ASPP(
+            in_channels=feature_maps[-1],
+            out_channels=aspp_channels,
+            atrous_rates=atrous_rates,
+            dropout=dropout,
+            activation=activation,
+        )
+        self.project = nn.Sequential(
+            nn.Conv2d(aspp_channels, aspp_channels, 3, padding=1, bias=False),
+            nn.BatchNorm2d(aspp_channels),
+            instantiate_activation_block(activation, inplace=True),
+            nn.Conv2d(aspp_channels, out_channels, kernel_size=1),
         )
 
-        self.dsv = nn.Conv2d(high_level_bottleneck, num_classes, kernel_size=1)
+        self._channels = [out_channels]
 
     def forward(self, feature_maps: List[Tensor]) -> List[Tensor]:
-        low_level_feat = feature_maps[0]
-        low_level_feat = self.conv1(low_level_feat)
-        low_level_feat = self.abn1(low_level_feat)
-
         high_level_features = feature_maps[-1]
         high_level_features = self.aspp(high_level_features)
+        return self.project(high_level_features)
 
-        mask_dsv = self.dsv(high_level_features)
+    @property
+    def channels(self) -> Tuple[int]:
+        return self._channels
+
+
+class DeeplabV3PlusDecoder(DecoderModule):
+    """
+    Implements DeepLabV3 model from `"Rethinking Atrous Convolution for Semantic Image Segmentation"
+    <https://arxiv.org/abs/1706.05587>`_.
+
+    Partially copy-pasted from https://github.com/pytorch/vision/blob/master/torchvision/models/segmentation/deeplabv3.py
+    """
+
+    def __init__(
+        self,
+        feature_maps: List[int],
+        aspp_channels: int,
+        out_channels: int,
+        atrous_rates=(12, 24, 36),
+        dropout: float = 0.5,
+        activation: str = ACT_RELU,
+        low_level_channels: int = 48,
+    ):
+        super().__init__()
+
+        self.project = nn.Sequential(
+            nn.Conv2d(feature_maps[0], low_level_channels, 1, bias=False),
+            nn.BatchNorm2d(low_level_channels),
+            instantiate_activation_block(activation, inplace=True),
+        )
+
+        self.aspp = ASPP(
+            in_channels=feature_maps[-1],
+            out_channels=aspp_channels,
+            atrous_rates=atrous_rates,
+            dropout=dropout,
+            activation=activation,
+            aspp_module=SeparableASPPModule,
+        )
+
+        self.final = nn.Sequential(
+            nn.Conv2d(aspp_channels + low_level_channels, out_channels, kernel_size=3, padding=1, bias=False,),
+            nn.BatchNorm2d(out_channels),
+            instantiate_activation_block(activation, inplace=True),
+        )
+        self._channels = [out_channels]
+
+    def forward(self, feature_maps: List[Tensor]) -> List[Tensor]:
+        low_level_features = self.project(feature_maps[0])
+        output_feature = self.aspp(feature_maps[-1])
 
         high_level_features = F.interpolate(
-            high_level_features, size=low_level_feat.size()[2:], mode="bilinear", align_corners=False
+            output_feature, size=low_level_features.shape[2:], mode="bilinear", align_corners=False
         )
-        high_level_features = torch.cat([high_level_features, low_level_feat], dim=1)
-        mask = self.last_conv(high_level_features)
+        combined_features = torch.cat([low_level_features, high_level_features], dim=1)
+        return [self.final(combined_features)]
 
-        return [mask, mask_dsv]
+    @property
+    def channels(self) -> Tuple[int]:
+        return self._channels
--- tests/test_decoders.py
+++ tests/test_decoders.py
@@ -4,10 +4,33 @@ import pytorch_toolbelt.modules.encoders as E
 import torch
 from pytorch_toolbelt.modules import FPNFuse
 from pytorch_toolbelt.modules.decoders import FPNSumDecoder, FPNCatDecoder
+from pytorch_toolbelt.modules import decoders as D
 from pytorch_toolbelt.utils.torch_utils import count_parameters
 from torch import nn
 
 skip_if_no_cuda = pytest.mark.skipif(not torch.cuda.is_available(), reason="CUDA is not available")
+
+
+@torch.no_grad()
+@pytest.mark.parametrize(
+    ("decoder_cls", "decoder_params"),
+    [
+        (D.FPNSumDecoder, {"fpn_channels": 128}),
+        (D.FPNCatDecoder, {"fpn_channels": 128}),
+        (D.DeeplabV3PlusDecoder, {"aspp_channels": 256, "out_channels": 256}),
+        (D.DeeplabV3Decoder, {"aspp_channels": 256, "out_channels": 256}),
+    ],
+)
+def test_decoders(decoder_cls, decoder_params):
+    channels = [64, 128, 256, 512, 1024]
+    input = [torch.randn((4, channels[i], 512 // (2 ** i), 512 // (2 ** i))).cuda() for i in range(len(channels))]
+    decoder = decoder_cls(channels, **decoder_params).cuda().eval()
+    output = decoder(input)
+
+    print(decoder.__class__.__name__)
+    print(count_parameters(decoder))
+    for o in output:
+        print(o.size())
 
 
 @torch.no_grad()
