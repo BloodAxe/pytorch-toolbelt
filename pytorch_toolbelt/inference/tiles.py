6946115de2c1cb829165ae8f5235b3a21538fde4
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2021-10-27 17:47:02 +0300

    Added check that batch is on the target device

--- pytorch_toolbelt/inference/tiles.py
+++ pytorch_toolbelt/inference/tiles.py
@@ -334,7 +334,9 @@ class TileMerger:
         if len(batch) != len(crop_coords):
             raise ValueError("Number of images in batch does not correspond to number of coordinates")
 
-        batch = batch.to(device=self.image.device)
+        if batch.device != self.image.device:
+            batch = batch.to(device=self.image.device)
+
         for tile, (x, y, tile_width, tile_height) in zip(batch, crop_coords):
             self.image[:, y : y + tile_height, x : x + tile_width] += tile * self.weight
             self.norm_mask[:, y : y + tile_height, x : x + tile_width] += self.weight
