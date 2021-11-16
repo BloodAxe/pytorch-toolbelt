283faaec8fbbf845f2f6a9708af931969611eb1d
BloodAxe <ekhvedchenya@gmail.com>
2021-11-16 13:09:08 +0200

    Added pretrained argument to GenericTimmEncoder

--- pytorch_toolbelt/modules/encoders/timm/common.py
+++ pytorch_toolbelt/modules/encoders/timm/common.py
@@ -11,14 +11,14 @@ __all__ = ["GenericTimmEncoder", "make_n_channel_input_std_conv"]
 
 
 class GenericTimmEncoder(EncoderModule):
-    def __init__(self, timm_encoder: Union[nn.Module, str], layers: List[int] = None):
+    def __init__(self, timm_encoder: Union[nn.Module, str], layers: List[int] = None, pretrained=True):
         strides = []
         channels = []
         default_layers = []
         if isinstance(timm_encoder, str):
             import timm.models.factory
 
-            timm_encoder = timm.models.factory.create_model(timm_encoder, pretrained=True)
+            timm_encoder = timm.models.factory.create_model(timm_encoder, pretrained=pretrained)
 
         for i, oi in enumerate(timm_encoder.feature_info.out_indices):
             fi = timm_encoder.feature_info.info[i]
