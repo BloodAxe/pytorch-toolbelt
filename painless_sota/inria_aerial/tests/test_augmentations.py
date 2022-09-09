478ed7e04c8cd25a36e33f9a7f8daf9cae21165a
BloodAxe <ekhvedchenya@gmail.com>
2022-09-09 23:46:52 +0300

    seg_r200d_unet_rdtsc_s2

--- painless_sota/inria_aerial/tests/test_augmentations.py
+++ painless_sota/inria_aerial/tests/test_augmentations.py
@@ -5,11 +5,25 @@ from hydra.utils import instantiate
 from omegaconf import OmegaConf
 
 import albumentations as A
+from skimage.measure import label
+from skimage.segmentation import find_boundaries
 
 from painless_sota.inria_aerial.data.image_io import read_tiff
 from painless_sota.inria_aerial.hydra_utils import register_albumentations_resolver
+from pytorch_toolbelt.utils import fs
 
-A.Perspective
+
+def test_dataset():
+    images = fs.find_images_in_dir("C:/datasets/inria/AerialImageDataset/train/images")
+    masks = fs.find_images_in_dir("C:/datasets/inria/AerialImageDataset/train/gt")
+
+    for image_path, mask in zip(images, masks):
+        boundaries = find_boundaries(label(read_tiff(mask)))
+        image = read_tiff(image_path)
+        image2 = image.copy()
+        image2[boundaries] = (255, 0, 255)
+        image = cv2.addWeighted(image, 0.5, image2, 0.5, 0)
+        cv2.imwrite(filename=fs.change_extension(os.path.basename(image_path), ".jpg"), img=image[..., ::-1])
 
 
 def test_augmentations():
