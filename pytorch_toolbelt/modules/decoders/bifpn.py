52f327b3045c5024cade6607c51d83b6cd2fa5ec
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2023-03-18 19:46:16 +0200

    Refactor BiFPNDecoder to take any number of feature maps as input. This is a BC since old implementation required only 3 feature maps and computed 2 additional inside. This is no longer the case.

--- pytorch_toolbelt/modules/decoders/bifpn.py
+++ pytorch_toolbelt/modules/decoders/bifpn.py
@@ -4,8 +4,12 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch import Tensor
-from .common import DecoderModule
-from ..activations import get_activation_block, ACT_RELU
+from pytorch_toolbelt.modules.decoders.common import DecoderModule
+from pytorch_toolbelt.modules.activations import (
+    ACT_RELU,
+    get_activation_block,
+    instantiate_activation_block,
+)
 
 __all__ = ["BiFPNDecoder", "BiFPNBlock", "BiFPNConvBlock", "BiFPNDepthwiseConvBlock"]
 
@@ -13,12 +17,20 @@ __all__ = ["BiFPNDecoder", "BiFPNBlock", "BiFPNConvBlock", "BiFPNDepthwiseConvBl
 class BiFPNDepthwiseConvBlock(nn.Module):
     """
     Depthwise seperable convolution.
-
-
     """
 
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, dilation=1, act=nn.ReLU):
+    def __init__(
+        self,
+        in_channels,
+        out_channels,
+        kernel_size=1,
+        stride=1,
+        padding=0,
+        dilation=1,
+        activation: str = ACT_RELU,
+    ):
         super(BiFPNDepthwiseConvBlock, self).__init__()
+
         self.depthwise = nn.Conv2d(
             in_channels, in_channels, kernel_size, stride, padding, dilation, groups=in_channels, bias=False
         )
@@ -27,7 +39,7 @@ class BiFPNDepthwiseConvBlock(nn.Module):
         )
 
         self.bn = nn.BatchNorm2d(out_channels, momentum=0.9997, eps=4e-5)
-        self.act = act(inplace=True)
+        self.act = instantiate_activation_block(activation, inplace=True)
 
     def forward(self, x: Tensor) -> Tensor:
         x = self.depthwise(x)
@@ -42,11 +54,12 @@ class BiFPNConvBlock(nn.Module):
 
     """
 
-    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, act=nn.ReLU, dilation=1):
+    def __init__(self, in_channels, out_channels, kernel_size=1, stride=1, padding=0, activation=ACT_RELU, dilation=1):
         super(BiFPNConvBlock, self).__init__()
+
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride=stride, padding=padding, bias=False)
         self.bn = nn.BatchNorm2d(out_channels, momentum=0.9997, eps=4e-5)
-        self.act = act(inplace=True)
+        self.act = instantiate_activation_block(activation, inplace=True)
 
     def forward(self, x: Tensor) -> Tensor:
         x = self.conv(x)
@@ -59,115 +72,125 @@ class BiFPNBlock(nn.Module):
     Bi-directional Feature Pyramid Network
     """
 
-    def __init__(self, feature_size: int = 64, epsilon: float = 0.0001, act=nn.ReLU):
+    def __init__(self, feature_size: int, num_feature_maps: int, epsilon=0.0001, activation=ACT_RELU):
         super(BiFPNBlock, self).__init__()
+
         self.epsilon = epsilon
+        num_blocks = num_feature_maps - 1
 
-        self.p3_td = BiFPNDepthwiseConvBlock(feature_size, feature_size, act=act)
-        self.p4_td = BiFPNDepthwiseConvBlock(feature_size, feature_size, act=act)
-        self.p5_td = BiFPNDepthwiseConvBlock(feature_size, feature_size, act=act)
-        self.p6_td = BiFPNDepthwiseConvBlock(feature_size, feature_size, act=act)
+        self.top_down_blocks = [
+            BiFPNDepthwiseConvBlock(feature_size, feature_size, activation=activation, act=act)
+            for _ in range(num_blocks)
+        ]
+        self.bottom_up_blocks = [
+            BiFPNDepthwiseConvBlock(feature_size, feature_size, activation=activation, act=act)
+            for _ in range(num_blocks)
+        ]
 
-        self.p4_out = BiFPNDepthwiseConvBlock(feature_size, feature_size, act=act)
-        self.p5_out = BiFPNDepthwiseConvBlock(feature_size, feature_size, act=act)
-        self.p6_out = BiFPNDepthwiseConvBlock(feature_size, feature_size, act=act)
-        self.p7_out = BiFPNDepthwiseConvBlock(feature_size, feature_size, act=act)
-
-        self.w1 = nn.Parameter(torch.Tensor(2, 4), requires_grad=True)
-        self.w1_relu = nn.ReLU()
-        self.w2 = nn.Parameter(torch.Tensor(3, 4), requires_grad=True)
-        self.w2_relu = nn.ReLU()
+        self.register_parameter("w1", nn.Parameter(torch.Tensor(2, num_blocks), requires_grad=True))
+        self.register_parameter("w2", nn.Parameter(torch.Tensor(3, num_blocks), requires_grad=True))
 
         torch.nn.init.constant_(self.w1, 1)
         torch.nn.init.constant_(self.w2, 1)
 
     def forward(self, inputs: List[Tensor]) -> List[Tensor]:
-        p3_x, p4_x, p5_x, p6_x, p7_x = inputs
+        transition_features = self.top_down_pathway(inputs)
+        return self.bottom_up_pathway(transition_features, inputs)
 
-        # Calculate Top-Down Pathway
-        w1 = self.w1_relu(self.w1)
-        w1 = w1 / (torch.sum(w1, dim=0) + self.epsilon)
+    def bottom_up_pathway(self, transition_features, inputs):
+        # Calculate Bottom-Up Pathway
+        # p3_out = p3_td
+        # p4_out = self.p4_out(
+        #     w2[0, 0] * p4_x + w2[1, 0] * p4_td + w2[2, 0] * F.interpolate(p3_out, size=p4_x.size()[2:])
+        # )
+        # p5_out = self.p5_out(
+        #     w2[0, 1] * p5_x + w2[1, 1] * p5_td + w2[2, 1] * F.interpolate(p4_out, size=p5_x.size()[2:])
+        # )
+        # p6_out = self.p6_out(
+        #     w2[0, 2] * p6_x + w2[1, 2] * p6_td + w2[2, 2] * F.interpolate(p5_out, size=p6_x.size()[2:])
+        # )
+        # p7_out = self.p7_out(
+        #     w2[0, 3] * p7_x + w2[1, 3] * p7_td + w2[2, 3] * F.interpolate(p6_out, size=p7_x.size()[2:])
+        # )
 
-        w2 = self.w2_relu(self.w2)
+        w2 = torch.nn.functional.relu(self.w2)
         w2 = w2 / (torch.sum(w2, dim=0) + self.epsilon)
 
-        p7_td = p7_x
-        p6_td = self.p6_td(w1[0, 0] * p6_x + w1[1, 0] * F.interpolate(p7_td, size=p6_x.size()[2:]))
-        p5_td = self.p5_td(w1[0, 1] * p5_x + w1[1, 1] * F.interpolate(p6_td, size=p5_x.size()[2:]))
-        p4_td = self.p4_td(w1[0, 2] * p4_x + w1[1, 2] * F.interpolate(p5_td, size=p4_x.size()[2:]))
-        p3_td = self.p3_td(w1[0, 3] * p3_x + w1[1, 3] * F.interpolate(p4_td, size=p3_x.size()[2:]))
+        outputs = [transition_features[-1]]
+        transition_reversed = transition_features[:-1][::-1]
+        for i, block in enumerate(self.bottom_up_blocks):
+            x = inputs[i + 1]
+            td = transition_reversed[i]
+            y = block(x * w2[0, i] + td * w2[1, i] + F.interpolate(outputs[-1], size=x.size()[2:]) * w2[2, i])
+            outputs.append(y)
 
-        # Calculate Bottom-Up Pathway
-        p3_out = p3_td
-        p4_out = self.p4_out(
-            w2[0, 0] * p4_x + w2[1, 0] * p4_td + w2[2, 0] * F.interpolate(p3_out, size=p4_x.size()[2:])
-        )
-        p5_out = self.p5_out(
-            w2[0, 1] * p5_x + w2[1, 1] * p5_td + w2[2, 1] * F.interpolate(p4_out, size=p5_x.size()[2:])
-        )
-        p6_out = self.p6_out(
-            w2[0, 2] * p6_x + w2[1, 2] * p6_td + w2[2, 2] * F.interpolate(p5_out, size=p6_x.size()[2:])
-        )
-        p7_out = self.p7_out(
-            w2[0, 3] * p7_x + w2[1, 3] * p7_td + w2[2, 3] * F.interpolate(p6_out, size=p7_x.size()[2:])
-        )
+        return outputs
 
-        return [p3_out, p4_out, p5_out, p6_out, p7_out]
+    def top_down_pathway(self, inputs: List[Tensor]) -> List[Tensor]:
+        # p6_td = self.p6_td(w1[0, 0] * p6_x + w1[1, 0] * F.interpolate(p7_td, size=p6_x.size()[2:]))
+        # p5_td = self.p5_td(w1[0, 1] * p5_x + w1[1, 1] * F.interpolate(p6_td, size=p5_x.size()[2:]))
+        # p4_td = self.p4_td(w1[0, 2] * p4_x + w1[1, 2] * F.interpolate(p5_td, size=p4_x.size()[2:]))
+        # p3_td = self.p3_td(w1[0, 3] * p3_x + w1[1, 3] * F.interpolate(p4_td, size=p3_x.size()[2:]))
+
+        w1 = torch.nn.functional.relu(self.w1)
+        w1 = w1 / (torch.sum(w1, dim=0) + self.epsilon)
+
+        features = [inputs[-1]]
+        inputs_reversed = inputs[:-1][::-1]
+
+        for i, block in enumerate(self.top_down_blocks):
+            x = inputs_reversed[i]
+            y = block(w1[0, i] * x + w1[1, i] * F.interpolate(features[-1], size=x.size()[2:]))
+            features.append(y)
+
+        return features
 
 
 class BiFPNDecoder(DecoderModule):
     """
-    BiFPN decoder
-
-    Expects input of three feature maps
+    BiFPN decoder. Note this class does not compute additional feature maps (p6/p7) and expects them as input.
+    Because of this it supports arbitrary number of input feature maps.
 
     Reference: https://arxiv.org/abs/1911.09070
     """
 
     def __init__(
-        self, feature_maps: List[int], strides: List[int], channels: int = 64, num_layers: int = 2, activation=ACT_RELU
+        self, feature_maps: List[int], strides: List[int], channels: int, num_layers: int, activation=ACT_RELU
     ):
         super(BiFPNDecoder, self).__init__()
-        act = get_activation_block(activation)
-        if len(feature_maps) != 3:
-            raise ValueError("Number of input feature maps must be equal 3")
-        self.p3 = nn.Conv2d(feature_maps[0], channels, kernel_size=(1, 1))
-        self.p4 = nn.Conv2d(feature_maps[1], channels, kernel_size=(1, 1))
-        self.p5 = nn.Conv2d(feature_maps[2], channels, kernel_size=(1, 1))
 
-        # p6 is obtained via a 3x3 stride-2 conv on C5
-        self.p6 = nn.Conv2d(feature_maps[2], channels, kernel_size=(3, 3), stride=(2, 2), padding=1)
+        self.projections = nn.ModuleList(
+            [nn.Conv2d(f, channels, kernel_size=1, stride=1, padding=0) for f in feature_maps]
+        )
 
-        # p7 is computed by applying ReLU followed by a 3x3 stride-2 conv on p6
-        self.p7 = BiFPNConvBlock(channels, channels, kernel_size=3, stride=2, padding=1, act=act)
-
-        bifpns: List[BiFPNBlock] = []
+        bifpns = []
         for _ in range(num_layers):
-            bifpns.append(BiFPNBlock(channels, act=act))
-        self.bifpn = nn.ModuleList(bifpns)
+            bifpns.append(BiFPNBlock(channels, num_feature_maps=len(feature_maps), activation=activation))
+        self.bifpn = nn.Sequential(*bifpns)
 
-        self._channels = [channels] * 5
-        self._strides = tuple(strides) + (strides[-1] * 2, strides[-1] * 4)
+        self._channels = [channels] * len(feature_maps)
+        self._strides = tuple(strides)
 
     @property
+    @torch.jit.ignore
     def channels(self):
         return self._channels
 
     @property
+    @torch.jit.ignore
     def strides(self):
         return self._strides
 
-    def forward(self, inputs: List[Tensor]) -> List[Tensor]:
-        c3, c4, c5 = inputs
-
+    def forward(self, feature_maps: List[Tensor]) -> List[Tensor]:
         # Calculate the input column of BiFPN
-        p3_x = self.p3(c3)
-        p4_x = self.p4(c4)
-        p5_x = self.p5(c5)
-        p6_x = self.p6(c5)
-        p7_x = self.p7(p6_x)
+        features = [p(c) for p, c in zip(self.projections, feature_maps)]
+        return self.bifpn(features)
 
-        features = p3_x, p4_x, p5_x, p6_x, p7_x
-        for bifpn in self.bifpn:
-            features = bifpn(features)
-        return features
+
+if __name__ == "__main__":
+    from pytorch_toolbelt.utils.torch_utils import maybe_cuda, count_parameters, describe_outputs
+
+    decoder = BiFPNDecoder(feature_maps=[256, 512, 1024], strides=[8, 16, 32], channels=256, num_layers=3)
+    inputs = [torch.randn(1, 256, 64, 64), torch.randn(1, 512, 32, 32), torch.randn(1, 1024, 16, 16)]
+    outputs = decoder(inputs)
+    print(describe_outputs(outputs))
