e48a9ac080cd747acb4e074f79addb97510ccbcd
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2023-07-08 20:58:25 +0300

    QFL docs

--- pytorch_toolbelt/losses/quality_focal_loss.py
+++ pytorch_toolbelt/losses/quality_focal_loss.py
@@ -4,6 +4,16 @@ from torch import nn, Tensor
 
 class QualityFocalLoss(nn.Module):
     def __init__(self, beta: float = 2, reduction="mean"):
+        """
+        Quality Focal Loss from https://arxiv.org/abs/2006.04388
+
+        :param beta: Power factor for focal term
+        :param reduction: Possible values are: mean, sum, normalized
+            mean - mean loss value is returned
+            sum - sum of loss values is returned
+            normalized - mean loss value is divided by sum of focal terms.
+            This mimics normalized focal loss approach.
+        """
         super().__init__()
         self.beta = beta
         self.reduction = reduction
@@ -11,10 +21,11 @@ class QualityFocalLoss(nn.Module):
     @torch.cuda.amp.autocast(False)
     def forward(self, predictions: Tensor, targets: Tensor) -> Tensor:
         """
+        Compute quality focal loss
 
-        :param predictions: Prediction Logits
-        :param targets: Targets
-        :return:
+        :param predictions: Prediction logits
+        :param targets: Targets of the same shape as predictions
+        :return: Loss value
         """
         predictions = predictions.float()
         targets = targets.float()
@@ -27,4 +38,7 @@ class QualityFocalLoss(nn.Module):
             return loss.mean()
         if self.reduction == "sum":
             return loss.sum()
+        if self.reduction == "normalized":
+            return loss.sum() / focal_term.sum()
+
         return loss
