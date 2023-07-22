82708c3c6aff182de60ca126eb29688327c72b85
BloodAxe <ekhvedchenya@gmail.com>
2023-07-22 14:05:39 +0300

    Added plot_heatmap

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from pytorch_toolbelt.utils import plot_confusion_matrix
+from pytorch_toolbelt.utils import plot_confusion_matrix, plot_heatmap
 
 
 def test_plot_confusion_matrix():
@@ -8,4 +8,11 @@ def test_plot_confusion_matrix():
 
     plot_confusion_matrix(
         cm, class_names=["A", "B", "C", "D", "E", "F", "G"], normalize=True, fname="test_plot_confusion_matrix.png"
+    )
+
+def test_plot_heatmap():
+    cm = np.random.randn(20, 30)
+
+    plot_heatmap(
+        cm, title="Test", x_label="30", y_label="20", fname="test_plot_heatmap.png", noshow=False
     )
