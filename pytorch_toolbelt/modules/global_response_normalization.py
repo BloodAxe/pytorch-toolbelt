056d3829e3af3cd3e8ec2383e2fb93d63530d210
BloodAxe <ekhvedchenya@gmail.com>
2023-10-15 09:41:10 +0300

    GlobalResponseNormalization2d

---
+++ pytorch_toolbelt/modules/global_response_normalization.py
@@ -0,0 +1,28 @@
+class GlobalResponseNormalization2d(nn.Module):
+    """
+    Global Response Normalization layer from ConvNextV2
+    https://arxiv.org/abs/2301.00808
+    """
+    
+    def __init__(self, in_channels: int, epsilon: float = 1e-8):
+        super().__init__()
+        self.in_channels = in_channels
+        self.gamma = nn.Parameter(torch.ones(1, in_channels, 1, 1))
+        self.beta = nn.Parameter(torch.zeros(1, in_channels, 1, 1))
+        self.epsilon = epsilon
+        
+        torch.nn.init.constant_(self.gamma, 1.0)
+        torch.nn.init.constant_(self.beta, 0.0)
+    
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        
+        Args:
+            x: Tensor of shape [B, C, H, W]
+
+        Returns:
+            Tensor of shape [B, C, H, W]
+        """
+        gx = torch.norm(x, p=2, dim=(2, 3), keepdim=True)
+        nx = gx / gx.mean(dim=-1, keepdim=True).add_(self.epsilon)
+        return self.gamma * (x * nx) + self.beta + X
